a5519b61df82b5958eda927f96fdf80f372034dc
Yemi Akinyemi <yemi2times@gmail.com>
2020-09-28 23:12:20 +0100

    Initial Commit

---
+++ ec2.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_instance" "my_ec2" {
+  count           = 2
+  ami             = "ami-0bb3fad3c0286ebd5"
+  subnet_id       = "subnet-42641b18"
+  instance_type   = "t2.micro"
+  key_name        = "bade4times"
+  security_groups = ["launch-wizard-6"]
+  tags = {
+    "Name" = "bade4times-${count.index + 1}"
+  }
+}
+
+output "ec2_id" {
+  value = aws_instance.my_ec2.*.id
+}
+
